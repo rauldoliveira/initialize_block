a7c4bc5658422ee1fbbffb6d0e380326c4a735b3
Raul Oliveira <raul.oliveira@msn.com>
2020-05-04 18:31:34 -0300

    First commit

---
+++ Makefile
@@ -0,0 +1,23 @@
+# note this must match version macros in initialize_block.h
+FD_VERSION=11.0
+
+PROGRAM = initialize_block
+#OBJS = initialize_block.o
+
+PG_CONFIG = pg_config
+PGXS := $(shell $(PG_CONFIG) --pgxs)
+include $(PGXS)
+
+# avoid linking against all libs that the server links against (xml, selinux, ...)
+LIBS = $(libpq_pgport)
+
+DISTFILES= Makefile Makefile.contrib \
+	initialize_block.c 
+
+dist:
+	rm -rf initialize_block-${FD_VERSION} initialize_block-${FD_VERSION}.tar.gz
+	mkdir initialize_block-${FD_VERSION}
+	cp -p ${DISTFILES} initialize_block-${FD_VERSION}
+	tar cfz initialize_block-${FD_VERSION}.tar.gz initialize_block-${FD_VERSION}
+	rm -rf initialize_block-${FD_VERSION}
+
---
+++ initialize_block.c
@@ -0,0 +1,114 @@
+#include "postgres.h"
+
+#include <string.h>
+#include <stdio.h>
+#include <time.h>
+#include <ctype.h>
+
+#include "storage/bufpage.h"
+
+FILE *fp = NULL;
+int segmentNumber;
+
+void
+PageInit(char *page, Size pageSize, Size specialSize)
+{
+	PageHeader	p = (PageHeader) page;
+
+	specialSize = MAXALIGN(specialSize);
+
+	Assert(pageSize == BLCKSZ);
+	Assert(pageSize > specialSize + SizeOfPageHeaderData);
+
+	/* Make sure all fields of page are zero, as well as unused space */
+	MemSet(p, 0, pageSize);
+
+	p->pd_flags = 0;
+	p->pd_lower = SizeOfPageHeaderData;
+	p->pd_upper = pageSize - specialSize;
+	p->pd_special = pageSize - specialSize;
+	PageSetPageSizeAndVersion(page, pageSize, PG_PAGE_LAYOUT_VERSION);
+	/* p->pd_prune_xid = InvalidTransactionId;		done by above MemSet */
+}
+
+static unsigned int
+GetSegmentNumberFromFileName(const char *fileName)
+{
+	int			segnumOffset = strlen(fileName) - 1;
+
+	if (segnumOffset < 0)
+		return 0;
+
+	while (isdigit(fileName[segnumOffset]))
+	{
+		segnumOffset--;
+		if (segnumOffset < 0)
+			return 0;
+	}
+
+	if (fileName[segnumOffset] != '.')
+		return 0;
+
+	return atoi(&fileName[segnumOffset + 1]);
+}
+
+
+int main(int argc, char *argv[]){
+
+    char arquivo[500]; 
+    int bloco;
+    
+    bloco = atoi(argv[2]); ;
+    strcpy(arquivo, argv[1]);
+
+    printf("Inicializando bloco = <%d> do arquivo = <%s>\n", bloco, arquivo);
+    
+    fp = fopen(arquivo, "r+b");
+	if (!fp){
+		printf("Nao foi possivel abrir <%s>.\n", arquivo);
+		exit(1);
+	}
+
+	segmentNumber = GetSegmentNumberFromFileName(arquivo);
+
+	int pageOffset = bloco % ((1024 * 1024 * 1024) / BLCKSZ);
+
+	char *block = (char *)malloc(BLCKSZ);
+	if (!block)
+	{
+		printf("\nErro: Nao foi possivel criar buffer do tamanho <%d>.\n",
+			   BLCKSZ);
+	}
+
+	unsigned int position = BLCKSZ * pageOffset;
+
+	if (fseek(fp, position, SEEK_SET) != 0)
+	{
+		printf("Error: Seek encontrou um erro antes do start block <%d>.\n", position);
+		exit(2);
+	}
+
+	fread(block, 1, BLCKSZ, fp);
+
+	PageInit(block,BLCKSZ, 0);
+
+	if (fseek(fp, position, SEEK_SET) != 0)
+	{
+		printf("Error: Seek encontrou um erro antes do start block <%d>.\n", position);
+		exit(2);
+	}
+
+	if (fwrite(block, 1, BLCKSZ, fp) != BLCKSZ)
+	{
+		printf("Erro ao escrever data file <%s>.\n", arquivo);
+		exit(3);
+	}
+	else
+	{
+		printf("Sucesso ao escrever data file <%s>.\n", arquivo);	
+	}
+
+	fclose(fp);
+
+	return 0;
+}
